1399d91a0c35daa3d93b000b95f8b60ca191f67a
HumbledByte <mariodmorales127@gmail.com>
2022-05-10 16:27:11 -0500

    updating meow.c

---
+++ meow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int i = 1;
+while (i <= 3)
+
+{ 
+    printf("Meow\n");
+    i++;
+}
+
+
+// for (i = 0; i < 3; i++)
+// {
+//   printf("meow\n");
+// }
+
+// Understanding while loops, for lopps and boolean operators
