7da690ad863c8b1ad787f0ab48447239e95b68bf
HumbledByte <mariodmorales127@gmail.com>
2022-05-10 12:17:03 -0500

    understadning how to use chars in C

--- agree.c
+++ agree.c
@@ -1,2 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
+
+// Understanding how to use Chars
+
+int main(void)
+// prompt user to agree
+
+{
+    char c = get_char("Do you agree ");
+
+    if (c == 'y'|| c == 'Y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n'||c == 'N')
+    {
+        printf("Not Agreed\n");
+    }
+}
