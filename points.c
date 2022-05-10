cd05357af052d405493318b342175e5670a3a1de
HumbledByte <mariodmorales127@gmail.com>
2022-05-09 22:11:37 -0500

    updating MINE variable

--- points.c
+++ points.c
@@ -7,11 +7,11 @@ int main(void)
     const int MINE = 2
     int points = get_int("How many points did you lose? ");
     
-    if (points < mine)
+    if (points < MINE)
     {
         printf(" You lost fewer points than me.\n");
     }
-    else if (points > mine)
+    else if (points > MINE)
     {
         printf("You lost more points than me\n");
     }
