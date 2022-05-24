efecc064649fb30ba096cb25e22d7d6ea227bb82
HumbledByte <mariodmorales127@gmail.com>
2022-05-24 12:58:58 -0500

    For loops with arrays

--- scores.c
+++ scores.c
@@ -3,6 +3,7 @@
 
 // Learning arrays
 
+/*
 int main(void)
 {
     int scores[3];
@@ -10,5 +11,23 @@ int main(void)
     scores[0] = 72;
     scores[1] = 73;
     scores[2] = 74;
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
+}
+*/
+
+
+
+// Using for loops with arrays
+
+int main(void)
+{
+    int n = get_int("How many scores do you have?: ");
+    int scores[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scores[i] = get_int("score?: ");
+    }
+
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
