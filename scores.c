f54644b1acebfc56f07bdc386cc50a71ad075a86
HumbledByte <mariodmorales127@gmail.com>
2022-05-24 12:22:59 -0500

    Learning how to use arrays

--- scores.c
+++ scores.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <cs50.h>
+
+// Learning arrays
 
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 74;
+    int scores[3];
 
-    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 74;
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
