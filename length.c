209966dab0c73bec7e8978e5eb292a999d564f6c
HumbledByte <mariodmorales127@gmail.com>
2022-05-31 18:06:52 -0500

    leaving comment

---
+++ length.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+// Iterating through a string
+
+
+int main(void)
+{   
+    string name = get_string("Name: ");
+
+    int i = 0;
+
+    while (name[i] != '\0')
+    {
+        i++;
+    }
+    
+    printf("%i\n", i);
+}
