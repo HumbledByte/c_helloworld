03e67b8469cca2b6f7bac8a4d831c29dfc1f6791
HumbledByte <mariodmorales127@gmail.com>
2022-06-13 21:46:36 -0500

    iterating over every character using for loop

--- string.c
+++ string.c
@@ -2,3 +2,14 @@
 #include <cs50.h>
 #include <string.h>
 
+// Iterating over every character in a string using a for loop
+
+int main(void)
+{
+    string s = get_string("Input: ");
+    printf("Output: ");
+    for (int i = 0; int n = strlen(s); i < n; i++)
+    {
+        printf("%c", s[i]);
+    }
+}
