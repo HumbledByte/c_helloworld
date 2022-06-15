aa0e4dc94a942277b06baa2ab3dfa9b7fd4c3a8a
HumbledByte <mariodmorales127@gmail.com>
2022-06-14 20:53:15 -0500

    Learning structs

--- phonebook.c
+++ phonebook.c
@@ -4,16 +4,28 @@
 
 // Using string compare, exit statuses and arrays
 
+typedef struct 
+{
+    string name;
+    string number;
+}
+person;
+
+
 int main(void)
 {
-    string names[] = {"Carter", "David"};
-    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
+    person people[2];
+    people[0].name = "Carter";
+    people[0].number = "773-123-4567";
+
+    people[1].name = "David";
+    people[1].number = "773-456-7891";
 
     for (int i = 0; i < 2; i++)
     {
-        if (strcmp(names[i], "Mario") == 0 )
+        if (strcmp(people[i].name, "Carter") == 0 )
         {
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
