e4f12cd968b3312f0cf0330ccfe26940ecd0e875
HumbledByte <mariodmorales127@gmail.com>
2022-06-16 21:57:25 -0500

    understanding pointers

---
+++ address.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Understanding how pointers work
+
+
+// 1st way to print an address
+int main(void)
+/*
+{
+    int n = 50;
+    int *p = &n;
+    printf("%p\n", p);
+}
+*/
+
+//How to print out what is at a specific address
+
+{
+    int n = 50;
+    int *p = &n;
+    printf("%p\n", p);
+    printf("%i\n", n);
+    //dereference operator
+    printf("%i\n", *p);
+
+}
+
